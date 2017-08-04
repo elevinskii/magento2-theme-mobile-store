684793a26793ad98c0f35d32f93fd3570e0fa8c1
Evgeny Levinsky <el@ideal-code.ru>
2017-08-04 12:56:40 +0300

    Solved minicart loading issues

--- app/design/frontend/IdealCode/MobileStore/Magento_Checkout/web/js/view/minicart-extend.js
+++ app/design/frontend/IdealCode/MobileStore/Magento_Checkout/web/js/view/minicart-extend.js
@@ -1,18 +1,27 @@
 define([
     'Magento_Checkout/js/view/minicart',
-], function (Component) {
+    'underscore'
+], function (Component, _) {
 
     return Component.extend({
         /**
-         * Get minicart label in Russian locale.
-         * @param {String} count
+         * Get minicart label.
          * @param titles
+         * @param locale
          * @returns {*}
          */
-        getLabel: function(count, titles) {
-            /** @see https://gist.github.com/realmyst/1262561 */
-            var cases = [2, 0, 1, 1, 1, 2];
-            return titles[ (count%100>4 && count%100<20)? 2 : cases[(count%10<5)?count%10:5] ];
+        getLabel: function(titles, locale = '') {
+            var count = this.getCartParam('summary_count');
+
+            if(!_.isUndefined(count)) {
+                if(locale == 'ru') {
+                    /** @see https://gist.github.com/realmyst/1262561 */
+                    var cases = [2, 0, 1, 1, 1, 2];
+                    return titles[(count % 100 > 4 && count % 100 < 20) ? 2 : cases[(count % 10 < 5) ? count % 10 : 5]];
+                } else {
+                    return titles[count == 1 ? 0 : 1];
+                }
+            }
         }
     });
 });
