c28106fe192cd7357d8d5d253f5da3d10c065a96
Evgeny Levinsky <el@ideal-code.ru>
2017-08-29 12:19:27 +0300

    Wishlist JS init fix

--- app/design/frontend/IdealCode/MobileStore/Magento_Wishlist/web/js/view/wishlist.js
+++ app/design/frontend/IdealCode/MobileStore/Magento_Wishlist/web/js/view/wishlist.js
@@ -1,8 +1,18 @@
 define([
-    'Magento_Wishlist/js/view/wishlist'
-], function (Component) {
+    'uiComponent',
+    'Magento_Customer/js/customer-data'
+], function (Component, customerData) {
 
     return Component.extend({
+
+        /**
+         * Init of UI component
+         */
+        initialize: function () {
+            this._super();
+            this.wishlist = customerData.get('wishlist');
+        },
+
         /**
          * Check product in wishlist
          * @param id
