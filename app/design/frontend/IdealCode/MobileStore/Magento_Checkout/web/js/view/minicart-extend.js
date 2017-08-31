95c2a73bbd0be1f90b359dcc37311f09bc814010
Evgeny Levinsky <el@ideal-code.ru>
2017-08-31 11:31:40 +0300

    Sidebar cart: main work

--- app/design/frontend/IdealCode/MobileStore/Magento_Checkout/web/js/view/minicart-extend.js
+++ app/design/frontend/IdealCode/MobileStore/Magento_Checkout/web/js/view/minicart-extend.js
@@ -1,25 +1,27 @@
 define([
+    'jquery',
     'Magento_Checkout/js/view/minicart',
     'underscore'
-], function (Component, _) {
+], function ($, Component, _) {
 
     return Component.extend({
+
         /**
          * Get minicart label.
-         * @param titles
          * @param locale
          * @returns {*}
          */
-        getLabel: function(titles, locale = '') {
-            var count = this.getCartParam('summary_count');
+        getLabel: function(locale = '') {
+            var count = this.getCartParam('summary_count'),
+                titles = [$.mage.__('item'), $.mage.__('items'), $.mage.__('items2')];
 
             if(!_.isUndefined(count)) {
-                if(locale == 'ru') {
+                if(locale == 'ru_RU') {
                     /** @see https://gist.github.com/realmyst/1262561 */
                     var cases = [2, 0, 1, 1, 1, 2];
-                    return titles[(count % 100 > 4 && count % 100 < 20) ? 2 : cases[(count % 10 < 5) ? count % 10 : 5]];
+                    return count+' '+titles[(count % 100 > 4 && count % 100 < 20) ? 2 : cases[(count % 10 < 5) ? count % 10 : 5]];
                 } else {
-                    return titles[count == 1 ? 0 : 1];
+                    return count+' '+titles[count == 1 ? 0 : 1];
                 }
             }
         }
