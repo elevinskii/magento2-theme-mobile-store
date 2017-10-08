7fe2736f1614568178a0db2c55bfdb7bae446d1a
Evgeny Levinsky <el@ideal-code.ru>
2017-10-08 10:43:51 +0300

    Toolbar: amount label

---
+++ app/code/IdealCode/Catalog/Block/Product/ProductList/Toolbar.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace IdealCode\Catalog\Block\Product\ProductList;
+
+class Toolbar extends \Magento\Catalog\Block\Product\ProductList\Toolbar
+{
+    /**
+     * @param $count
+     * @return string
+     */
+    public function getAmountLabel($count)
+    {
+        $titles = [__('item'), __('items'), __('items2')];
+        $locale = $this->_scopeConfig->getValue(
+            \Magento\Directory\Helper\Data::XML_PATH_DEFAULT_LOCALE,
+            \Magento\Store\Model\ScopeInterface::SCOPE_STORE
+        );
+
+        if($locale == 'ru_RU') {
+            /** @see https://gist.github.com/realmyst/1262561 */
+            $cases = [2, 0, 1, 1, 1, 2];
+            return $count.' '.$titles[($count % 100 > 4 && $count % 100 < 20) ? 2 : $cases[($count % 10 < 5) ? $count % 10 : 5]];
+        } else {
+            return $count.' '.$titles[$count == 1 ? 0 : 1];
+        }
+    }
+}
