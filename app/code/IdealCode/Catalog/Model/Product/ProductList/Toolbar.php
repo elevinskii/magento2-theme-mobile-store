1271cdbc3be69dee2a802b5c8e1b0b48aaac1f6f
Evgeny Levinsky <el@ideal-code.ru>
2017-10-09 08:06:13 +0300

    Toolbar: sorter

--- app/code/IdealCode/Catalog/Model/Product/ProductList/Toolbar.php
+++ app/code/IdealCode/Catalog/Model/Product/ProductList/Toolbar.php
@@ -4,7 +4,25 @@ namespace IdealCode\Catalog\Model\Product\ProductList;
 
 class Toolbar extends \Magento\Catalog\Model\Product\ProductList\Toolbar
 {
+    const ORDER_PARAM_NAME = 'order';
+    const DIRECTION_PARAM_NAME = 'dir';
     const LIMIT_PARAM_NAME = 'limit';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getOrder()
+    {
+        return $this->request->getParam(self::ORDER_PARAM_NAME);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getDirection()
+    {
+        return $this->request->getParam(self::DIRECTION_PARAM_NAME);
+    }
 
     /**
      * {@inheritdoc}
